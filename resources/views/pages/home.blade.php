da2bb2baa76712855534baf5343aa0a85ead416f
Henrique <henriqueferreira0320@gmail.com>
2025-01-21 17:30:47 -0300

    Fonts, variables and font awesome

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        @include('../includes/assets-header')
+
         <title>Assetech</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
         <!-- Styles / Scripts -->
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+        @vite(['resources/scss/pages/home.scss', 'resources/js/app.js'])
     </head>
     <body>
         home
