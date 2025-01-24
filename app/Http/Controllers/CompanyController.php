571c532c72e3a1df598f8c133f9825a53d1c76a4
Henrique <henriqueferreira0320@gmail.com>
2025-01-24 16:34:55 -0300

    Linking sections with database

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -11,11 +11,17 @@ class CompanyController extends GeneralController
     public function __invoke()
     {
         $testimonials = $this->getTestimonials();
+        $ourResumeWithoutFormat = $this->getArticle('Article Our Resume Company');
+        $ourResume = explode('<br/>', $ourResumeWithoutFormat->content);
+
+        $ourMission = $this->getArticle('Article Our Mission Company');
 
         return view('pages.company', [
             'companyLinks' => CompanyLink::all(),
             'companySocialMedia' => CompanySocialMedia::all(),
             'testimonials' => $testimonials,
+            'ourResume' => $ourResume,
+            'ourMission' => $ourMission,
         ]);
     }
 }
--- resources/views/pages/company.blade.php
+++ resources/views/pages/company.blade.php
@@ -22,14 +22,13 @@
         {{-- Parallax --}}
         <section class="parallax"></section>
     
-        <section class="our-resume">
-            <p>Somos uma empresa comprometida com a excelência, dedicada a fornecer serviços e equipamentos de alta qualidade para atender às necessidades específicas dos nossos clientes. Fundada em [ano de fundação], nossa jornada tem sido marcada por inovação, integridade e compromisso com a satisfação do cliente.</p>
-            <p>No cerne da nossa filosofia está o compromisso com a qualidade. Desde a concepção até a entrega, cada aspecto do nosso trabalho reflete padrões rigorosos e atenção aos detalhes. Nossa equipe talentosa e dedicada, composta por profissionais apaixonados e experientes, é o alicerce do nosso sucesso.</p>
-            <p>Na Assetech, acreditamos na inovação contínua. Estamos constantemente explorando novas maneiras de melhorar nossos serviços e oferecer soluções inovadoras aos nossos clientes. Essa mentalidade orientada para o futuro nos permite permanecer na vanguarda da indústria de serviços e equipamentos.</p>
-            <p>Além disso, valorizamos profundamente a relação com nossos clientes. Estamos comprometidos em construir parcerias duradouras, baseadas na confiança mútua e comunicação transparente. A satisfação do cliente é a medida definitiva do nosso sucesso. Como uma empresa socialmente responsável, também nos dedicamos a causas que impactam positivamente nossa comunidade e o meio ambiente. Estamos empenhados em fazer a diferença e contribuir para um futuro sustentável.</p>
-            <p>Agradecemos por escolher a Assetech como seu parceiro em serviços e equipamentos. Estamos entusiasmados em compartilhar nossa história, nossos valores e nosso compromisso com você. Juntos, estamos construindo um caminho de sucesso e crescimento.
-            Seja parte da nossa jornada. Seja parte da Assetech.</p>
-        </section>
+        @if($ourResume)
+            <section class="our-resume">
+                @foreach($ourResume as $resume)
+                    {!! $resume !!}
+                @endforeach
+            </section>
+        @endif
 
         
         {{-- Info Icons --}}
@@ -38,6 +37,19 @@
         {{-- Testimonials --}}
         @if($testimonials)
             <x-testimonials :testimonials="$testimonials" />
+        @endif
+
+        {{-- Our Mission --}}
+        @if($ourMission)
+            <section class="our-mission">
+                <div class="our-mission__wrapper">
+                    <p class="default_subtitle">{{ $ourMission->subtitle }}</p>
+                    <h2 class="default_title">{{ $ourMission->title }}</h2>
+                    <div class="paragraph">
+                        <p>{{ $ourMission->content }}</p>
+                    </div>
+                </div>
+            </section>
         @endif
 
         <x-footer :companyLinks="$companyLinks" :companySocialMedia="$companySocialMedia" />
